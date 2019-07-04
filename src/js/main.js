670d9d561bffddb8f409887b81c5fcb5f8416fcd
Shohrux <dotanoobbich@mail.ru>
2019-07-04 14:37:10 +0500

    Added lazyload, removed jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 let mediumZoom = require("medium-zoom");
 let images = document.querySelectorAll("#img");
+// let $ = require("jquery");
 
 images.forEach(image => mediumZoom(image));
+
+lazyload(images);
